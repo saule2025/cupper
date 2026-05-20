6a95c8049366da69aa114feb1772d2c4b36188f5
unknown <service.tek.bot@mail.ru>
2026-05-20 12:34:15 +0500

    Diploy

--- cupper/settings.py
+++ cupper/settings.py
@@ -90,19 +90,19 @@ WSGI_APPLICATION = 'cupper.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-
     'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'cupper',
-       'USER' : 'customer',
-       'PASSWORD' : 'customer',
-       'HOST' : '127.0.0.1',
-       'PORT' : '5433',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+       # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       # 'NAME': 'cupper',
+       # 'USER' : 'customer',
+       # 'PASSWORD' : 'customer',
+       # 'HOST' : '127.0.0.1',
+       # 'PORT' : '5433',
+    # }
 
     # 'default': {
 	   #  'ENGINE': 'django.db.backends.postgresql_psycopg2',
